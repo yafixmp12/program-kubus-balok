13862d15f277721167d87a93ea9c29314d0c7c1c
yafixmp12 <113622206+yafixmp12@users.noreply.github.com>
2022-09-15 22:42:27 +0700

    program

---
+++ python kubus balok/balok.py
@@ -0,0 +1,7 @@
+#volume balok
+print("menghitung volume balok")
+p = int(input("masukkan panjang balok ="))
+l = int(input("masukkan luas balok ="))
+t = int(input("masukkan tinggi balok ="))
+volumebalok = (2*p*l)+(2*p*t)+(2*l*t)
+print("volume baloknya adalah", volumebalok)
---
+++ python kubus balok/kubus.py
@@ -0,0 +1,5 @@
+#volume kubus
+print("menghitung volume kubus")
+s = int(input("masukkan panjang rusuk kubus ="))
+volumekubus = s * s * s
+print("volume kubusnya adalah", volumekubus)
